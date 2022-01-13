8d8a5f681dd5b27ba6d2e7770965e342803afdb0
Ben Campagnola <bcampagnola@fortherecord.com>
2022-01-13 15:08:39 -0800

    added branch "sheet_test", with new Directory.cs, and changes to UI_Manager.cs

---
+++ Main_Template/Directory.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FTR_Utils;
+using VC = MastersHelperLibrary.VirtualConsole;
+using System.Data;
+
+namespace Main_Template
+{
+    class Directory
+    {
+        public Sheet data;
+        public List<Entry> book;
+
+        public Directory(string _fileName)
+        {
+            book = new List<Entry>();
+            data = new Sheet();
+            VC.Send(data.LoadFile(_fileName));
+        }
+
+        public uint ReadTable()
+        {
+            if (!data.bFileLoaded)
+                return 0;
+            foreach (DataRow r in data.result.Tables[0].Rows)
+            {
+                Entry e = new Entry(r);
+                if (e.bIsInitialized)
+                    book.Add(e);
+            }
+            return 0;
+        }
+    }
+
+    class Entry
+    {
+        public bool bIsInitialized = false;
+        public static List<EntryItem> items = new List<EntryItem>();
+
+        public static string[] columns = new string[] {    "UID",
+                                            "RoomName",
+                                            "FriendlyName",
+                                            "Location",
+                                            "Group",
+                                            "Phone01",
+                                            "Phone02",
+                                            "Processor_IP",
+                                            "DSP_IP",
+                                            "Cam01_IP",
+                                            "Cam02_IP",
+                                            "Cam01_URL",
+                                            "Cam02_URL",
+                                            "Cam01_Presets",
+                                            "Cam02_Presets" };
+
+        public Entry()
+        {
+            InitItems();
+        }
+        public Entry(DataRow r)
+        {
+            InitItems();
+
+            for (int i = 0; i > r.ItemArray.Count(); i++)
+            {
+                items[i].ParseField(r.ItemArray[i].ToString());
+            }
+        }
+
+        public void InitItems()
+        { 
+            foreach (var v in columns)
+                items.Add(new EntryItem(v.Trim().ToLower(), typeof(string)));
+            items[0].t = typeof(int);
+
+            if(items[0].s_val.Contains("//") == false)
+                bIsInitialized = true;
+        }
+    }
+
+    public class EntryItem
+    {
+        public string s_val;
+        public int i_val;
+        public Type t;
+        public string fieldName;
+
+        public EntryItem(string _fieldName, Type _t)
+        {
+            fieldName = _fieldName;
+            t = _t;
+        }
+        public void ParseField(string s)
+        {
+            s_val = s.ToString();
+            if (t == typeof(int))
+                if (int.TryParse(s.Trim(), out int i))
+                    i_val = i;
+        }
+    }
+}
--- Main_Template/UI_Manager.cs
+++ Main_Template/UI_Manager.cs
@@ -7,6 +7,7 @@ using Crestron.SimplSharpPro.UI;
 using Crestron.SimplSharpPro.DeviceSupport;
 using Crestron.SimplSharpPro;
 using FTR_Utils;
+using VC = MastersHelperLibrary.VirtualConsole;
 
 namespace Main_Template
 {
@@ -23,9 +24,56 @@ namespace Main_Template
         {
             cs = _cs;
 
-            AddPanel(new XpanelForSmartGraphics(0x20, cs));
-            AddPanel(new XpanelForSmartGraphics(0x21, cs));
-            AddPanel(new XpanelForSmartGraphics(0x22, cs));
+
+            //AddPanel(new XpanelForSmartGraphics(0x20, cs));
+            //AddPanel(new XpanelForSmartGraphics(0x21, cs));
+            //AddPanel(new XpanelForSmartGraphics(0x22, cs));
+
+            VC.AddNewConsoleCommand(VC_Register, "reg", "");
+            VC.AddNewConsoleCommand(VC_LoadSG, "loadsg", "");
+            VC.AddNewConsoleCommand(VC_RegisterSG, "regsg", "");
+        }
+
+        public string VC_Register(string s)
+        {
+            try
+            {
+                panel.Add(0x20, new XpanelForSmartGraphics(0x20, cs));
+                panel[0x20].Register();
+                panel[0x20].SigChange += panel_SigChange;
+
+            }
+            catch(Exception e)
+            {
+                VC.Send(String.Format(">>> {0}", e));
+            }
+            return "";
+        }
+        public string VC_LoadSG(string s)
+        {
+            uint i;
+            try
+            {
+                i = panel[0x20].LoadSmartObjects(String.Format("{0}{1}", filePath, panelStation_sgd_file));
+                if (i > 0)
+                    for (uint j = 1; j <= i; j++)
+                    {
+                        VC.Send(String.Format(">>> {0}", panel[0x20].SmartObjects[j].ToString()));
+                        panel[0x20].SmartObjects[j].SigChange += panel_SigChange_SG;
+
+                    }
+                else VC.Send(">>> NumOf SG Loaded = 0");
+            }
+            catch (Exception e)
+            {
+                VC.Send(String.Format(">>> {0}", e));
+            }
+            return "";
+        }
+
+        public string VC_RegisterSG(string s)
+        {
+            return "";
         }
 
         public void AddPanel(BasicTriListWithSmartObject _panel)
@@ -37,9 +85,12 @@ namespace Main_Template
             uint i;
 
             panel.Add(id, _panel);
-            i = panel[id].LoadSmartObjects(String.Format("{0}{1}", filePath, panelStation_sgd_file));
-            
+
+            panel[id].Register();
+
             panel[id].SigChange += panel_SigChange;
+
+            i = panel[id].LoadSmartObjects(String.Format("{0}{1}", filePath, panelStation_sgd_file));
             if (i > 0)
                 for (uint j = 1; j <= i; j++)
                     panel[id].SmartObjects[j].SigChange += panel_SigChange_SG;
@@ -48,17 +99,20 @@ namespace Main_Template
 
         private void panel_SigChange(BasicTriList b, SigEventArgs args)
         {
+            //VC.Send(">>> in panel_SigChange");
             panel_SigInfo(args.Sig.Type, args.Sig.Number, 0, b.ID);
         }
 
         private void panel_SigChange_SG(GenericBase b, SmartObjectEventArgs args)
         {
+            //VC.Send(">>> in panel_SigChange_SG");
             panel_SigInfo(args.Sig.Type, args.Sig.Number, args.SmartObjectArgs.ID, b.ID);
         }
 
         private void panel_SigInfo(eSigType _type, uint _n, uint _smartID, uint _panelID)
         {
             PanelSigInfo psi = new PanelSigInfo(_type, _n, _panelID);
+            VC.Send(String.Format(">>> PanelSig - type={0} n={1} sgID={2} panelID={3}", _type, _n, _smartID, _panelID));
         }
 
         public string SigValue(SigEventArgs args)
